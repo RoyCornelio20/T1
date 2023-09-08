57f58819284635e4ff7522de1666e5ae9c6e5a55
Roy <142622768+RoyCornelio20@users.noreply.github.com>
2023-09-08 13:48:58 -0500

    Add files via upload
    Examen T1 Orlandini

---
+++ src/roy/Persona.java
@@ -0,0 +1,28 @@
+package roy;
+
+
+
+
+public class Persona {
+    private String Nombre;
+ 
+    public Persona()
+    {}
+    
+    public Persona(String Nombre)
+    {   this.Nombre = Nombre;
+
+    }
+
+      public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+   
+      public String getNombre() {
+        return Nombre;
+    }
+
+   
+    
+}
---
+++ src/roy/Roy.java
@@ -0,0 +1,18 @@
+package roy;
+
+
+public class Roy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         Lista Ventana = new Lista();
+        Ventana.setLocationRelativeTo(Ventana);
+        Ventana.setVisible(true); 
+        
+        
+    }
+    
+}
